a2b26b0e45492ec7676c070b4fa1db8f9b32abc9
Roeurnvanneth <roeurn.vanneth@institute.pse.ngo>
2024-12-20 16:08:59 +0700

    first code

--- index.js
+++ index.js
@@ -3,32 +3,44 @@
 
 // Task 1: Comment Your JavaScript Code
 // Write a single-line comment below this line:
-
+console.log("Hello world");
+result = 3 + 7 
 // Task 2: Declare JavaScript Variables
 // Declare a variable named `myName`:
-
+ var namae = "Myname";
 // Task 3: Storing Values with the Assignment Operator
 // Assign the value "JavaScript" to a variable named `myLanguage`:
 
 // Task 4: Assigning the Value of One Variable to Another
 // Declare a variable named `a` with the value 5, and another variable `b`. Assign `a` to `b`:
+var a;
+a = 5;
+b = 5 ;
+var a = b;
 
 // Task 5: Initializing Variables with the Assignment Operator
 // Declare and initialize a variable named `myAge` with the value 25:
+var Myage = 25;
 
 // Task 6: Declare String Variables
 // Declare a string variable named `greeting` and set it to "Hello, World!":
-
+var name ="greeting" = "Hello, World";
 // Task 7: Understanding Uninitialized Variables
 // Declare three variables `x`, `y`, and `z` without assigning values. Then, assign values 1, 2, and 3 to them:
-
+var x =1;
+var y = 2;
+var z = 3;
 // Task 8: Understanding Case Sensitivity in Variables
 // Fix the variable names to follow case sensitivity:
 // let StUdEnTnAmE = "John";
 // let sTuDeNtAgE = 20;
+let StudentTname = "Jonh";
+let StudentAge = 20;
 
 // Task 9: Explore Differences Between the var and let Keywords
 // Declare a variable using `var` and another using `let`. Show the difference in scope (use a block of code to demonstrate):
 
 // Task 10: Declare a Read-Only Variable with the const Keyword
 // Declare a constant variable `PI` and assign it the value 3.14:
+var name ="PI"
+var PI = 3.14;
